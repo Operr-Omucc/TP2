81ab3af5169da10796844741489d6ce57c36ac6f
Operr-Omucc <francomarchiori2006@gmail.com>
2025-05-14 15:30:37 -0300

    Juamba = ej8, Facundo = ej7 Franco = ej6 (y el resto entre todos)

--- ej8.php
+++ ej8.php
@@ -1,3 +1,21 @@
 <?php
-$NUM= [ 3, 2, 1, 5, 4]
+$NUM= [ 3, 2, 1, 5, 4];
+$n = count($NUM);
+
+for ($i = 1; $i < $n; $i++) {
+    $key = $NUM[$i];
+    $j = $i - 1;
+
+
+    while ($j >= 0 && $NUM[$j] > $key) {
+        $NUM[$j + 1] = $NUM[$j];
+        $j--;
+    }
+    $NUM[$j + 1] = $key;
+}
+
+for ($i = 0; $i < $n; $i++) {
+    echo $NUM[$i]. ",";
+}
+
 ?>
