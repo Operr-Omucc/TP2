1494ce9b447b03d810e04a56bfa6ba0cd79708f3
Hiikael <notarealwaifu@gmail.com>
2025-05-14 14:22:04 -0300

    Ej7 (Facu)

--- ej7.php
+++ ej7.php
@@ -1,15 +1,18 @@
 <?php
-$NUM= [ 3, 2, 1, 5, 4];
-$aux = $NUM[1];
-for ($i=0;$i<5;$i++){
-    for ($i=0;$i<5;$i++){
-        if ($aux>$NUM[$i]){
-            $aux=$NUM[$i];
+$NUM = [3, 2, 1, 5, 4];
+
+for ($i = 0; $i < count($NUM) - 1; $i++) {
+    for ($j = 0; $j < count($NUM) - 1 - $i; $j++) {
+        if ($NUM[$j] > $NUM[$j + 1]) {
+            $temp = $NUM[$j];
+            $NUM[$j] = $NUM[$j + 1];
+            $NUM[$j + 1] = $temp;
         }
     }
-    
 }
-for ($i=0;$i<5;$i++){
-print($NUM[$i]);
+
+echo "Arreglo ordenado: ";
+for ($i = 0; $i < count($NUM); $i++) {
+    echo $NUM[$i] . " ";
 }
 ?>
